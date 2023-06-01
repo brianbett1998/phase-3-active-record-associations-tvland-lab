351b6e45deafa37d56aa06c9491593c4b2ec57ee
BRIAN BETT <+>
2023-06-01 10:37:03 +0300

    completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters 
+    has_many :shows, through: :characters
+
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end  
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        self.characters.collect do |char|
+            "#{char.name} - #{char.show.name}"
+        end.join
+    end
 end
--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,13 @@
 class Network < ActiveRecord::Base
-  
+    has_many :shows
+
+    # def shows
+    #     self.shows.collect do |show|
+    #         "#{show.name}"
+    #     end.join
+    # end  
+
+    def sorry
+        "We're sorry about passing on John Mulaney's pilot"
+    end
 end
